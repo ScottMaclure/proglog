8ecac368aa8f22b6e363a781adcd6d1cbc48aad4
Scott Maclure <scott.maclure@sourcedgroup.com>
2020-10-19 21:38:18 +0800

    Testing

--- main.go
+++ main.go
@@ -5,7 +5,22 @@ import (
 	"github.com/ScottMaclure/proglog/internal/server"
 )
 
+func appendLog(log *server.Log, message string) {
+	r := server.Record{Value: []byte("Hello, World!")}
+	offset, err := log.Append(r)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("offset:", offset)
+	fmt.Println("Log:", log)
+}
+
 func main() {
 	log := server.NewLog()
 	fmt.Println("Log:", log)
+
+	appendLog(log, "Hello, World")
+	appendLog(log, "Goodbye, World")
+	appendLog(log, "From Hell's heart, I stab at thee...")
+
 }
